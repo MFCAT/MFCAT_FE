c18591b67da2baa19c28f7d936f942f89a927a38
MFCAT <mfcatofficial@gmail.com>
2021-11-21 17:17:53 +0000

    routerv6 added to navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Topbar from './components/Topbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Topbar from "./components/Topbar";
+import MarketPlace from "./components/MarketPlace";
+import Account from "./components/Account";
 import Create from "./components/Create";
 
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="bgimage">
       <Topbar />
-      <Create />
-  
-    
-      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<MarketPlace />}></Route>
+          <Route path="/marketplace" element={<MarketPlace />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/account" element={<Account />}></Route>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- src/components/MarketPlace.js
+++ src/components/MarketPlace.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Account = () => {
+const MarketPlace = () => {
   return (
     <div className="container text-white">
-      <h1>Account</h1>
+      <h1>MarketPlace</h1>
     </div>
   );
 };
 
-export default Account;
+export default MarketPlace;
